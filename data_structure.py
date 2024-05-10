5ef09b70c9f4de443ac9459e1fd31c945110e2f1
Umar Farooq <113279701+chumar77@users.noreply.github.com>
2024-05-10 12:00:59 +0500

    Update data_structure.py

--- data_structure.py
+++ data_structure.py
@@ -1,1 +1,36 @@
+#Multipul value listed
+list_of_num =[]
+list_of_num.append(1)
+list_of_num.append(2)
+list_of_num.append(3)
+list_of_num.append("Umar")
+
+print("list_of_num")
+
+#Arry use listed
+list_of_cloud =["aws","azure","gcp","orcal"]
+list_of_evn=["dev","test","prod"]
+
+#list Iteration
+
+for cloud in list_of_cloud:
+    if cloud =="aws":
+        print("AWS is best cloud")
+
+#dictionary
+dict_of_cloud ={
+    "aws":"Amazon Web Services",
+    "auzre": "Micosoft Azure",
+    "gcp":"Google Cloud Platfrom"
+}
+
+print(dict_of_cloud.get("azure","Not Found"))
+print(dict_of_cloud ["aws"])
+#add dictionary
+dict_of_cloud.update({"linode":"linode cloud "})
+print(dict_of_cloud)
+
+
+
+
 
